14b6bb14886297cc9c2bac9e4bd788b8b6a80809
TGOU <Alice@10.1.9.22>
2013-04-05 16:50:09 +0200

    e

--- carbuProject/CarteGenerale.php
+++ carbuProject/CarteGenerale.php
@@ -4,5 +4,5 @@ require_once 'application/inc/declarations.inc.php';
 $body = 'cartegenerale.body.php';
 echo Structure::getHeader();
 echo Structure::getBody($body);
-//LOLILOL TOTO TITI TATA
+// RECHANGE
 ?>
