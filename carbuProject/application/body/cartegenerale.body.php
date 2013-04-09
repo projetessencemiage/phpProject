0cdccebd6a047ef7db558f558e27c57448062f13
TGOU <Alice@10.1.9.22>
2013-04-09 13:52:44 +0200

    Modif Form

--- carbuProject/application/body/cartegenerale.body.php
+++ carbuProject/application/body/cartegenerale.body.php
@@ -4,35 +4,25 @@
 <div  id="formGoogleMaps">
 <form action="toto.php" method="post" >
 <fieldset>
- <legend> A propos des CSS : </legend>
-   <p>Savez-vous ce que veut dire CSS ? : </p>
-     <input type="radio" name="CSS" value="oui" id="oui"
-     checked="checked" />
-     <label for="oui" class="inline">oui</label>
-     <input type="radio" name="CSS" value="non" id="non" />
-     <label for="non" class="inline">non</label>
-	 
-  <label for="utilise">Si oui, les utilisez-vous plutôt : </label>
-   <select name="utilise" id="utilise">
-   <option value="toujours"> toujours</option>
-   <option value="parfois"> parfois</option>
-   <option value="jamais"> jamais</option>
-   </select>
+ <legend> Se situer : </legend>
+  <label for="newAdresse">Adresse / Ville :</label>
+   <input type="text" name="newAdresse" size="20" 
+   maxlength="40" value="Tapez ici une adresse" id="newAdresse" />
 </fieldset>
 
 <fieldset>
- <legend>Vos coordonnées :</legend>
-  <label for="email">Votre email :</label>
-   <input type="email" name="email" size="20" 
-   maxlength="40" value="email" id="email" />
-
-  <label for="comments">Vos commentaires :</label>
-   <textarea name="comments" id="comments" cols="20" rows="4">
-   </textarea>
+ <legend>Paramètres :</legend>
+ <label for="carburantType">Choix du carburant : </label>
+   <select name="carburantType" id="carburantType">
+   <option value="0"> (Tous) </option>
+   <option value="1"> Gazoil </option>
+   <option value="2"> SP-95</option>
+   <option value="3"> GPL</option>
+   </select>
 </fieldset>
 
  <p>
- <input type="submit" value="Envoyer" />
+ <input type="submit" value="Lancer la recherche" />
  <input type="reset" value="Annuler" />
  </p>
 
