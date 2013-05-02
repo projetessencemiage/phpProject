ebe2983c4b5302d80e469e729f20f8d229f0a029
TGOU <Thom@10.3.4.83>
2013-05-02 14:59:08 +0200

    Gestion Station AJAX

--- carbuProject/js/validerStation.js
+++ carbuProject/js/validerStation.js
@@ -1,7 +1,33 @@
 function validerStation(idStation) {
-	alert('Valider ' + idStation);
+	getXhr();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var chaine = xhr.responseText;
+			var rep = chaine.split('|');
+			if (rep[0] == 'OK'){
+				
+			}
+			else alert('ERREUR' + xhr.responseText);
+		}
+	}
+	xhr.open("POST", 'gestionStationAjax.php', true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("action=validerStation&idStation=" + idStation);
 }
 
 function refuserStation(idStation) {
-	alert('Refuser ' + idStation);
+	getXhr();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var chaine = xhr.responseText;
+			var rep = chaine.split('|');
+			if (rep[0] == 'OK'){
+				
+			}
+			else alert('ERREUR' + xhr.responseText);
+		}
+	}
+	xhr.open("POST", 'gestionStationAjax.php', true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("action=annulerStation&idStation=" + idStation);	
 }
