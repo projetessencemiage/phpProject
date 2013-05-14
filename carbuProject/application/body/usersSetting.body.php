755920b3ac55061ef7d93b21d5ff18fb91ce6a5f
matthisG <matthis.guinebault@gmail.com>
2013-05-14 09:39:01 +0200

    Change user

---
+++ carbuProject/application/body/usersSetting.body.php
@@ -0,0 +1,59 @@
+<?php
+require_once 'ListeUsersClass.inc.php';
+
+
+//Recherche de la liste a afficher
+if (array_key_exists('actionForm', $_POST) && $_POST['actionForm'] != "") {
+	Fonctions::inputHidden('actionForm', $_POST['actionForm']);
+	if ($_POST['actionForm'] == "searchVille") {
+		Fonctions::inputHidden('searchVille', $_POST['searchVille']);
+		Fonctions::inputHidden('searchVilleDpt', $_POST['searchVilleDpt']);
+		$ville = $_POST["searchVille"];
+		$dpt = $_POST["searchVilleDpt"];
+		$listeStation->getStationsByVille($ville, $dpt, $carbuType);
+		$critere = 'Recherche par ville - '.$ville. ' ('.$dpt.')';
+	}
+}
+
+?>
+<h3>Gestion des utilisateurs</h3>
+
+
+<?php	
+	//Tableau des users
+	$listUsers = new listeUsersClass();
+	$users = $listUsers->getListUsersArray();
+
+echo "<table id=\"tablesorter-demo\" class=\"tablesorter\" border=\"0\" cellpadding=\"0\" cellspacing=\"1\">
+	<thead>
+		<tr>
+			<th>genre</th>
+			<th>nom</th>
+			<th>prenom</th>
+			<th>pseudo</th>
+			<th>email</th>
+			<th>adresse</th>
+			<th>code postal</th>
+			<th>ville</th>
+			<th>role</th>
+			<th>supprimer</th>";
+		echo "</tr>
+	</thead><tbody>";
+		
+		foreach ($users as $user){
+			echo "<tr>
+					<td>".$user->getCiv()."</td>
+					<td>".$user->getNom()."</td>
+					<td>".$user->getPrenom()."</td>
+					<td>".$user->getUserName()."</td>
+					<td>".$user->getMail()."</td>
+					<td>".$user->getAdresse()."</td>
+					<td>".$user->getCp()."</td>
+					<td>".$user->getVille()."</td>
+					<td>".$user->getRole()."</td>
+				</tr>";
+		}
+
+echo "</tbody></table>";
+Fonctions::inputHidden('actionForm', '');
+?>
---
+++ carbuProject/application/class/ListeUsersClass.inc.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * ------------------------------------------------------------------------
+ * @Name : ListeUsersClass.inc.php
+ * @Desc : Classe listeUsersClass
+ * @Author : Bimbo
+ * @Date : 12/04/2013 : création
+ * @Version : V1.0;
+ * ------------------------------------------------------------------------
+ **/
+
+class listeUsersClass {
+	private $listeUsers;
+
+	function __construct() {
+		$this->soapClient = new SoapClient("http://projetm2miage.no-ip.biz:8084/ActionAdmin.svc?wsdl", array('encoding'=>'UTF-8','trace'=>1));
+		$this->listeUsers = array();
+	}
+	
+	public function getListUsers(){
+		return $this->getListUsersArray();
+	}
+	
+	public function getListUsersArray() {
+		
+		try {
+		$this->soapClient->ListUtilisateur();
+		$result = $this->soapClient->__getLastResponse();
+			$dom = new DomDocument();
+			$dom->loadXML($result);
+			$tab = $dom->getElementsByTagName('User');
+			foreach ($tab as $user){
+				$adress = $user->childNodes->item(0)->nodeValue;
+				$civilite = $user->childNodes->item(2)->nodeValue;
+				$cp = $user->childNodes->item(3)->nodeValue;
+				$email = $user->childNodes->item(4)->nodeValue;
+				$nom = $user->childNodes->item(8)->nodeValue;
+				$prenom = $user->childNodes->item(9)->nodeValue;
+				$pseudo = $user->childNodes->item(10)->nodeValue;
+				$role = $user->childNodes->item(11)->childNodes->item(1)->nodeValue;
+				$ville = $user->childNodes->item(12)->nodeValue;
+								
+				$this->listeUsers[] = new User('', $pseudo, '', $civilite, $nom, $prenom, $adress, $cp, $ville, $email, $role, '', '', '');
+			}
+			
+			return $this->listeUsers;
+		}
+		catch(Exception $e) {
+			echo "Erreur Service";
+			return false;
+		};
+		return $arrayPrice;
+	}
+	
+}
+?>
